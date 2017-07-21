3d785dd104492faab0373cea3ba783cf1cdb2303
seigle15 <seigle15@gmail.com>
2017-07-20 22:30:49 -0700

    add projectile code

---
+++ ForScience/Assets/Scripts/ProjectileControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileControl : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2D;
+	// Use this for initialization
+	//Code from unity tortorial space shooter
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+		rb2D.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
