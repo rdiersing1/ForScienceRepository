0ee885cea13bbf98321929b12815c53994dc443b
Robert Diersing <rdiersin@usc.edu>
2017-08-11 21:29:07 -0700

    spawns obsticals and acid

--- ForScience/Assets/Scripts/MasterControlers/EntitySpawner.cs
+++ ForScience/Assets/Scripts/MasterControlers/EntitySpawner.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class EntitySpawner : MonoBehaviour { 
 
-    public float monsterLife = 1.0f;
+    public float entityLife = 1.0f;
     public float chunkSize = 1.0f;
     public float nextChunckAt = 1.0f;
     public float renderDistFromPlayer = 1.0f;
-    public float PDFModifierInverse = 1.0f;
-    public float ground = 1.0f;
+    public float trapRadius = 1.0f;
     public Rigidbody2D playerRB;
     public GameObject master;
     ScoreCounter sc;
     
     private GameObject monsterPrefab;
+    private GameObject obsticalPrefab;
+    private GameObject acidPrefab;
+    private List<float> traps = new List<float>();
 
 	void Start () {
         sc = master.GetComponent<ScoreCounter>();
         monsterPrefab = Resources.Load("Monster") as GameObject;
+        obsticalPrefab = Resources.Load("obstical") as GameObject;
+        acidPrefab = Resources.Load("acid") as GameObject;
 	}
 	
 	void Update () {
@@ -34,19 +38,57 @@ public class EntitySpawner : MonoBehaviour {
 
         // Assigns the number of monsters to spawn to an event from the pdf f(x) = x/(c * int((1/c) * s) ds from 0 to max)
         int maxNumMonsters = Mathf.CeilToInt(((Mathf.Sqrt(score) / 5) + 2));
-        float M = (1 / (2 * PDFModifierInverse)) * Mathf.Pow(maxNumMonsters, 2);
+        float M = 0.5f * Mathf.Pow(maxNumMonsters, 2);
         float rand = Random.Range(0.0f, 1.0f);
-        int numMonstersToSpawn = Mathf.CeilToInt(Mathf.Sqrt(2 * PDFModifierInverse * M * rand));
+        int numMonstersToSpawn = Mathf.CeilToInt(Mathf.Sqrt(2 * M * rand));
 
         // Spawn Monsters
-        float distBetweenMonsters = chunkSize / (numMonstersToSpawn + 1);
-        for (int i = 0; i < numMonstersToSpawn; ++i) {
-            GameObject monster = Instantiate(monsterPrefab) as GameObject;
-            monster.GetComponent<Rigidbody2D>().transform.position = new Vector2(location + (distBetweenMonsters * i), ground);
-        }
+        spawner(location, numMonstersToSpawn, monsterPrefab);
 
-        print("score: " + score + "\n" +
-              "max Monsters: " + maxNumMonsters + "\n" +
-              "Spawn: " + numMonstersToSpawn);
+        // spawn obsticals 
+        int numObsticalsToSpawn = Mathf.FloorToInt(Random.Range(0, 3));
+        spawner(location, numObsticalsToSpawn, obsticalPrefab);
+        print("Attempting obsticals: " + numObsticalsToSpawn);
+
+        // spawn acids
+        int numAcidToSpawn = Mathf.FloorToInt(Random.Range(0, 2));
+        spawner(location, numAcidToSpawn, acidPrefab);
+        print("Attempting acids: " + numAcidToSpawn);
+    }
+
+    // Spawns certian number of objects, does not spawn an "impossible object combo"
+    private void spawner(float location, int numObjects, GameObject objectPrefab) {
+        for (int i = 0; i < numObjects; ++i) {
+            float objPosX = Random.Range(location, location + chunkSize);
+            if (objectPrefab.CompareTag("Trap")) {
+                if (validTrapPos(objPosX)) {
+                    traps.Add(objPosX);
+                    spawnObject(objPosX, objectPrefab);
+                } else {
+                    print("invalid trap pos at: " + objPosX);
+                }
+            }
+            else {
+                spawnObject(objPosX, objectPrefab);
+            }
+        }
+    }
+
+    // Spawns a single object at objPosX for entityLife time
+    private void spawnObject(float objPosX, GameObject objectPrefab) {
+        GameObject obj = Instantiate(objectPrefab) as GameObject;
+        float objPosY = obj.transform.position.y;
+        obj.transform.position = new Vector3(objPosX, objPosY, 0);
+        Destroy(obj, entityLife);
+    }
+
+    // Detects if the current trap is in range of another trap
+    private bool validTrapPos(float position) {
+        for (int i = 0; i < traps.Count; ++i) {
+            if (position - traps[i] < trapRadius) {
+                return false;
+            }
+        }
+        return true;
     }
 }
